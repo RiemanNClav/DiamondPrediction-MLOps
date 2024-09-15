3218b22399daf4ca2ba17905159188700add1949
AngelClavellina <angel.chavez.clavellina@gmail.com>
2024-09-15 10:57:21 -0600

    update2

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def predict_datapoint():
                                clarity=clarity)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80)
+    app.run(debug=True, host="0.0.0.0",port=80)
